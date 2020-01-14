a62f56732b264005b44a8a87f8f4cdffecac2869
Atsuhiro <y79fbded72gszda@gmail.com>
2020-01-14 18:40:44 +0900

    rails g controller Lists index show new edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
