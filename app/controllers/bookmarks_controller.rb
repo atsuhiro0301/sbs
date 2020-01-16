964a7b4a2a740a897697eb76a56b59c9988248aa
Atsuhiro <y79fbded72gszda@gmail.com>
2020-01-16 19:27:57 +0900

    new registration

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,34 @@
 class BookmarksController < ApplicationController
   def index
+    @bookmarks = Bookmark.all
   end
 
   def show
   end
 
   def new
+    @bookmark = Bookmark.new
   end
 
   def edit
   end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to @bookmark
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def set_bookmark
+      @bookmark = Bookmark.find(params[:id])
+    end
+
+    def bookmark_params
+      params.require(:bookmark).permit(:url, :title, :category, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookmarks/index'
-  get 'bookmarks/show'
-  get 'bookmarks/new'
-  get 'bookmarks/edit'
+  get    'bookmarks/index', to:'bookmarks#index'
+  get    'bookmarks/show',  to:'bookmarks#show'
+  get    'bookmarks/new',   to:'bookmarks#new'
+  get    'bookmarks/edit',  to:'bookmarks#edit'
+  post   'bookmarks/index',   to:'bookmarks#create'
+  resources :bookmarks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
